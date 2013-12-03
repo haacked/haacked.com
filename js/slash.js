1ce7c38bf1fe9c9091b81e36cfd74b61959688eb
Haacked <haacked@gmail.com>
2013-12-02 21:43:15 -0800

    More style clean up and bug fixes
    Former-commit-id: 01fbbe8d56da3e3dc7d6cc1ef75677f251749967

--- js/slash.js
+++ js/slash.js
@@ -1,27 +1,19 @@
 (function($) {
 	// Append caption after pictures
-	$('.entry-content').each(function(i) {
-		var _i = i;
-		$(this).find('img').each(function() {
-			var alt = this.alt;
+	$('.entry-content img').each(function(i) {
+		var alt = this.alt;
+		var parent = $(this).parent();
 
-			var parent = $(this).parent();
+		if (alt != '') {
+			var element = $(this);
+			if (parent.is('a')) {
+				element = parent;
+			}
+			element.after('<span class="caption">'+alt+'</span>');
+		}
 
-			if (alt != '') {
-				var element = $(this);
-				if (parent.is('a')) {
-					element = parent;
-				}
-				element.after('<span class="caption">'+alt+'</span>');
-			}
-
-			if (!parent.is('a')) {
-				$(this).wrap('<a href="'+this.src+'" title="'+alt+'" class="fancybox" rel="gallery'+_i+'" />');
-			}
-			else {
-				parent.addClass('fancybox');
-				parent.attr('rel', 'gallery' + _i)
-			}
-		});
+		if (!parent.is('a')) {
+			$(this).wrap('<a href="'+this.src+'" title="'+alt+'" />');
+		}
 	});
 })(jQuery);
